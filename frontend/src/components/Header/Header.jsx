6ad32578dbd6c3534f2acb739590bb0942eb3783
Sina Abolhasani <sinaabolhasani077@gmail.com>
2025-12-09 00:16:56 +0330

    file new adminPanel

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <>
+      <div>
+        <div>
+          <img src="./img/search-normal" alt="" />
+          <input type="text" placeholder="جستجوی کالا" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
